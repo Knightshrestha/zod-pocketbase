8fe9423d008971284af9735d354ffd83d39e7dfe
Knightshrestha <copyrekxh759@gmail.com>
2025-08-15 17:03:33 +0545

    use proper pocketbase endpoint

--- package/src/sdk.ts
+++ package/src/sdk.ts
@@ -6,7 +6,7 @@ let adminPocketbase: Pocketbase;
 export async function getPocketbase({ adminEmail, adminPassword, url }: Credentials) {
   if (!adminPocketbase) {
     const pocketbase = new Pocketbase(url);
-    await pocketbase.admins.authWithPassword(adminEmail, adminPassword);
+    await pocketbase.collection('_superusers').authWithPassword(adminEmail, adminPassword);
     adminPocketbase = pocketbase;
   }
   return adminPocketbase;
