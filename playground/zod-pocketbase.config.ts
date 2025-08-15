8d63f0cd2621e088a9da5c18934e7733bf5a3a1b
Knightshrestha <copyrekxh759@gmail.com>
2025-08-15 17:19:16 +0545

    chore: update version

--- playground/zod-pocketbase.config.ts
+++
@@ -1,8 +0,0 @@
-import { pascalCase } from "es-toolkit";
-import type { Config } from "zod-pocketbase";
-
-export default {
-  ignore: ["users"],
-  nameRecordSchema: (name: string) => `z${pascalCase(name)}Record`,
-  output: "./src/lib/pocketbase.ts",
-} satisfies Config;
